eb6f4c6b8de10610037ff8268db880bc92a3f26f
Nguyen Anh Minh <n.a.minh1106@gmail.com>
2023-08-21 18:30:58 +0200

    supabase setup

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,14 +1,6 @@
 import { NextResponse } from "next/server";
 
 console.log("hello world");
-// export default (req, res) => {
-//   const data = {
-//     message: "podcast stuff",
-//     timestamp: new Date().toISOString(),
-//   };
-
-//   res.status(200).json(data);
-// };
 
 export async function GET(request: Request) {
   const data = {
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,15 @@
 // 👆 ettol client oldalon fut a kod, nem a szerveren
 import Image from "next/image";
 import Link from "next/link";
+import {createClient} from '@supabase/supabase-js'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+)
+
 
 const getData = async () => {
   try {
@@ -24,9 +33,19 @@ export default async function Home() {
   const data = await getData();
   console.log(data);
 
+  const supabase = createServerComponentClient({ cookies });
+  const { data: users } = await supabase.from("users").select();
+  console.log(users)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>wow such empty</div>
+      <ul className="my-auto text-foreground">
+        {users?.map((user:any) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <div>hello</div>
     </main>
   );
 }
