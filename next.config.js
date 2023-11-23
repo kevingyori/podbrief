371515c3364139cf1f5f6c850b2759b37cbc8fd2
Kevin Gyori <gyorijonatan@gmail.com>
2023-11-23 08:15:49 +0000

    remove console logs from prod build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 };
 
 module.exports = million.next(
